97405f3b51d363d355d058fd777f5761ab9944a4
Vyacheslav Krachkov <v.krachkov@enestech.net>
2021-12-11 20:01:22 +0200

    chg: use `request.scheme` instead of `request.get_raw_uri()`

--- drf_spectacular_extensions/postprocessing_hooks.py
+++ drf_spectacular_extensions/postprocessing_hooks.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from urllib.parse import urlparse
 
 from drf_spectacular.generators import SchemaGenerator
 from rest_framework.request import Request
@@ -12,6 +11,5 @@ def add_servers(
     public: bool,
 ) -> dict:
     if request:
-        parsed_uri = urlparse(request.get_raw_uri())
-        result["servers"] = [{"url": f"{parsed_uri.scheme}://{request.get_host()}/"}]
+        result["servers"] = [{"url": f"{request.scheme}://{request.get_host()}/"}]
     return result
